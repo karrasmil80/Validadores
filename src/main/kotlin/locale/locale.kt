4282fd58c0b4110b790ab3a1a246ef323c9fae44
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 00:53:46 +0100

    Creacion del archivo locale con la configuración del pais

--- src/main/kotlin/locale/locale.kt
+++ src/main/kotlin/locale/locale.kt
@@ -1,2 +1,20 @@
 package org.example.locale
 
+import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
+
+private val myLocale = Locale("es", "ES")
+
+fun LocalDate.toLocalDate(): String {
+    return this.format(
+        DateTimeFormatter
+            .ofLocalizedDate(FormatStyle.SHORT).withLocale(myLocale)
+    )
+}
+
+fun Double.toLocalMoney(): String {
+    return NumberFormat.getCurrencyInstance(myLocale).format(this)
+}
