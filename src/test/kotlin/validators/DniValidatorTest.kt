eb5251795311bed100fc40d3e6d1377577884236
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 11:41:04 +0100

    Creación de los test

--- src/test/kotlin/validators/DniValidatorTest.kt
+++ src/test/kotlin/validators/DniValidatorTest.kt
@@ -1,28 +1,111 @@
 package validators
 
+import org.example.validators.DniValidator
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
- class DniValidatorTest {
+import org.junit.jupiter.api.DisplayName
+import java.time.LocalDateTime
+
+class DniValidatorTest {
+
+    private val validator = DniValidator()
 
 @Test
- fun validateCodigo() {}
+@DisplayName("Debe validar correctamente un código de DNI válido")
+    fun validateCodigo() {
+
+        // arrangement
+        val expected = "12345678A"
+
+        // act
+        val resultado = expected
+
+       // expected
+       assertEquals(expected, resultado, "Código DNI: correcta")
+
+    }
 
 @Test
- fun validateNombre() {}
+@DisplayName("El nombre del Dni debería de estar relleno")
+    fun validateNombre() {
+        // arrangement
+        val expected = "Nombre"
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Casilla nombre: correcta")
+    }
 
 @Test
- fun validateApellido() {}
+@DisplayName("El apellido del DNI debería de estar relleno")
+    fun validateApellido() {
+        // arrangement
+        val expected = "Apellido"
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Casilla apellido: correcta")
+    }
 
 @Test
- fun validateFechaEmision() {}
+@DisplayName("La fecha de emisión debería de ser la fecha actual")
+    fun validateFechaEmision() {
+
+        // arrangement
+        val expected = LocalDateTime.now()
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Casilla fecha emision: correcta")
+    }
 
 @Test
- fun validateFechaNacimiento() {}
+@DisplayName("La fecha de nacimiento debería de ser la fecha actual")
+    fun validateFechaNacimiento() {
+
+        // arrangement
+        val expected = LocalDateTime.now()
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Casilla fecha nacimiento: correcta")
+    }
 
 @Test
- fun validateFechaCaducidad() {}
+@DisplayName("La fecha de caducidad debería de ser menor al día actual")
+    fun validateFechaCaducidad() {
+
+        // arrangement
+        val expected = LocalDateTime.now().minusDays(1)
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Casilla fecha caducidad: correcta")
+    }
 
 @Test
- fun validateNacionalidad() {}
+@DisplayName("La nacionalidad del DNI debería de estar rellena")
+    fun validateNacionalidad() {
+
+        // arrangement
+        val expected = "Nacionalidad"
+
+        // act
+        val resultado = expected
+
+        // expected
+        assertEquals(expected, resultado, "Nacionalidad: correcta")
+    }
+    
 }
