48fdc6b0e354a76940be1617cfa384c3002ac726
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 01:35:00 +0100

    Ejecución del proyecto

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,108 +16,69 @@ fun main() {
         apellido = "Martínez",
         fechaEmisión = LocalDate.now(),
         fechaNacimiento = LocalDate.of(1970, 1, 1),
-        fechaCaducidad = LocalDate.now().plusYears(20),
+        fechaCaducidad = LocalDate.now(),
         sexo = Dni.Tipo.MASCULINO,
         nacionalidad = "España"
     )
 
+    // validador de Dni
     val validator = DniValidator()
 
+    // Validador de código
     try {
         validator.validateCodigo(dni.código)
+        println("Código DNI: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
+    // Validador de nombre
     try {
         validator.validateNombre(dni.nombre)
+        println("Casilla nombre: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
+    // Validador de apellido
     try {
         validator.validateApellido(dni.apellido)
+        println("Casilla apellido: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
+    // Validador de fecha de emision
     try {
         validator.validateFechaEmision(dni.fechaEmisión)
+        println("Casilla fecha emisión : correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
+    // Validador de fecha de nacimiento
     try {
         validator.validateFechaNacimiento(dni.fechaNacimiento)
+        println("Casilla fecha nacimiento: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
-    /* try {
-        validator.validateFechaCaducidad(dni.fechaCaducidad)
+     // validador de fecha de caducidad
+     try {
+         validator.validateFechaCaducidad(dni.fechaCaducidad)
+         println("Casilla fecha caducidad: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
-     */
-
+    // Validador de nacionalidad
     try {
         validator.validateNacionalidad(dni.nacionalidad)
+        println("Casilla nacionalidad: correcta")
     } catch (e: DniExceptions.ValidationException) {
         println(e.message)
     }
 
-    // Dni inválido
-    val dniInválido = Dni (
-        código = "12345678ABC",
-        nombre = "Pepe",
-        apellido = "Martínez",
-        fechaEmisión = LocalDate.now(),
-        fechaNacimiento = LocalDate.of(3054, 1, 1),
-        fechaCaducidad = LocalDate.now().plusYears(20),
-        sexo = Dni.Tipo.MASCULINO,
-        nacionalidad = "España"
-    )
-
-    try {
-        validator.validateCodigo(dni.código)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateNombre(dni.nombre)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateApellido(dni.apellido)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateFechaEmision(dni.fechaEmisión)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateFechaNacimiento(dni.fechaNacimiento)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateFechaCaducidad(dni.fechaCaducidad)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
-
-    try {
-        validator.validateNacionalidad(dni.nacionalidad)
-    } catch (e: DniExceptions.ValidationException) {
-        println(e.message)
-    }
 }
+
