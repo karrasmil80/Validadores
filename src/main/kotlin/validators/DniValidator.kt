1d068d0c256f190c7bbdf1cb1aceb4eb585bba15
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 01:19:37 +0100

    Creación del validador del Dni

--- src/main/kotlin/validators/DniValidator.kt
+++ src/main/kotlin/validators/DniValidator.kt
@@ -1,65 +1,67 @@
 package org.example.validators
+
 import org.example.exceptions.DniExceptions
 import org.example.models.Dni
 import java.time.LocalDate
 
 class DniValidator {
 
-    fun validate(dni: Dni) {
-
-        // Dni, 8 números 1 letra
+    // Validar código del DNI
+    fun validateCodigo(código: String) {
         val regex = "^[0-9]{8}[A-Za-z]$".toRegex()
-        if(!regex.matches(dni.código)) {
-            DniExceptions.ValidationException("El formato del Dni es incorrecto")
-        }
-
-        // Nombre, no puede estar vacío
-        if (dni.nombre.isBlank()) {
-            DniExceptions.ValidationException("El nombre no puede estar en blanco")
-        }
-
-        // Apellido, no puede estar vacío
-        if (dni.apellido.isBlank()) {
-            DniExceptions.ValidationException("El apellido no puede estar en blanco")
-        }
-
-        // Sexo, no puede estar vacío
-        if (Dni.Tipo.values().isEmpty()) {
-            DniExceptions.ValidationException("El tipo no puede estar en blanco")
-        }
-
-        // Fecha emisión, no puede ser superior a la actual
-        if (dni.fechaEmisión > LocalDate.now()) {
-            DniExceptions.ValidationException("La fecha de emisión no puede ser mayor a la actual")
-        }
-
-        // Fecha emisión, no puede estar vacía
-        if (dni.fechaEmisión.toString().isBlank())
-            DniExceptions.ValidationException("La fecha de emisión no puede estar en blanco")
-
-        // Fecha de nacimiento, no puede ser superior a la actual
-        if (dni.fechaNacimiento > LocalDate.now()) {
-            DniExceptions.ValidationException("La fecha de nacimiento no puede ser mayor a la actual")
-        }
-
-        // Fecha de nacimiento, no puede estar vacía
-        if (dni.fechaNacimiento.toString().isBlank()) {
-            DniExceptions.ValidationException("La fecha de nacimiento no puede estar en blanco")
-        }
-
-        // Fecha de caducidad, no puede estar vacía
-        if (dni.fechaCaducidad.toString().isBlank())
-            DniExceptions.ValidationException("La fecha de caducidad no puede estar en blanco")
-
-        // Fecha de caducidad, no puede ser superior a la actual
-        if (dni.fechaCaducidad > LocalDate.now() ) {
-            DniExceptions.ValidationException("La fecha de caducidad no puede ser mayor a la actual")
-        }
-
-        // Nacionalidad, no puede estar vacía
-        if (dni.nacionalidad.isBlank()) {
-            DniExceptions.ValidationException("La nacionalidad no puede estar en blanco")
+        if (!regex.matches(código)) {
+            throw DniExceptions.ValidationException("El formato del DNI es incorrecto")
         }
     }
 
+    // Validar Nombre
+    fun validateNombre(nombre: String) {
+        if (nombre.isBlank()) {
+            throw DniExceptions.ValidationException("El nombre no puede estar en blanco")
+        }
+    }
+
+    // Validar apellido
+    fun validateApellido(apellido: String) {
+        if (apellido.isBlank()) {
+            throw DniExceptions.ValidationException("El apellido no puede estar en blanco")
+        }
+    }
+
+    //Validar fecha de emisión
+    fun validateFechaEmision(fechaEmision: LocalDate) {
+        if (fechaEmision > LocalDate.now()) {
+            throw DniExceptions.ValidationException("La fecha de emisión no puede ser mayor a la actual")
+        }
+        if (fechaEmision.toString().isBlank()) {
+            throw DniExceptions.ValidationException("La fecha de emisión no puede estar en blanco")
+        }
+    }
+
+    // Validar fecha de nacimiento
+    fun validateFechaNacimiento(fechaNacimiento: LocalDate) {
+        if (fechaNacimiento > LocalDate.now()) {
+            throw DniExceptions.ValidationException("La fecha de nacimiento no puede ser mayor a la actual")
+        }
+        if (fechaNacimiento.toString().isBlank()) {
+            throw DniExceptions.ValidationException("La fecha de nacimiento no puede estar en blanco")
+        }
+    }
+
+    // Validar fecha de caducidad
+    fun validateFechaCaducidad(fechaCaducidad: LocalDate) {
+        if (fechaCaducidad.toString().isBlank()) {
+            throw DniExceptions.ValidationException("La fecha de caducidad no puede estar en blanco")
+        }
+        if (fechaCaducidad > LocalDate.now()) {
+            throw DniExceptions.ValidationException("La fecha de caducidad no puede ser mayor a la actual")
+        }
+    }
+
+    // Validar nacionalidad
+    fun validateNacionalidad(nacionalidad: String) {
+        if (nacionalidad.isBlank()) {
+            throw DniExceptions.ValidationException("La nacionalidad no puede estar en blanco")
+        }
+    }
 }
