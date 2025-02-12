c6a9f3876e75ef708990e26094f02845e798a786
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 12:01:25 +0100

    Main.kt
    Ejecución del programa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,6 @@ import org.example.models.Dni
 import org.example.validators.DniValidator
 import java.time.LocalDate
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
     // Prueba con Dni válido
