68c98bf29a22f678f4aa9f01b52d2a0ee5aaaf66
Pablo Zuil <zupablo3@gmail.com>
2025-02-12 10:36:42 +0100

    Creación de los test

---
+++ src/test/kotlin/validators/DniValidatorTest.kt
@@ -0,0 +1,28 @@
+package validators
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class DniValidatorTest {
+
+@Test
+ fun validateCodigo() {}
+
+@Test
+ fun validateNombre() {}
+
+@Test
+ fun validateApellido() {}
+
+@Test
+ fun validateFechaEmision() {}
+
+@Test
+ fun validateFechaNacimiento() {}
+
+@Test
+ fun validateFechaCaducidad() {}
+
+@Test
+ fun validateNacionalidad() {}
+}
